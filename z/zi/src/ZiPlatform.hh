102bfa91daa1045774e3e1f10cbf498ff50e294c
Huw Rogers <djnz00@gmail.com>
2024-05-08 05:38:51 +0900

    comment

--- z/zi/src/ZiPlatform.hh
+++ z/zi/src/ZiPlatform.hh
@@ -4,7 +4,7 @@
 // (c) Copyright 2024 Psi Labs
 // This code is licensed by the MIT license (see LICENSE for details)
 
-// platform primitives
+// platform-specific I/O primitives
 
 #ifndef ZiPlatform_HH
 #define ZiPlatform_HH
