730b607bb13faa7d03ba5d6d3dbfe0c457f442ed
Huw Rogers <djnz00@gmail.com>
2024-05-09 10:37:18 +0900

    added missing zu_lib.h

---
+++ z/zu/src/zu_lib.h
@@ -0,0 +1,158 @@
+//  -*- mode:c++; indent-tabs-mode:t; tab-width:8; c-basic-offset:2; -*-
+//  vi: noet ts=8 sw=2 cino=+0,(s,l1,m1,g0,N-s,j1,U1,W2,i2
+
+// (c) Copyright 2024 Psi Labs
+// This code is licensed by the MIT license (see LICENSE for details)
+
+// Z library main header - C API
+
+#ifndef zu_lib_H
+#define zu_lib_H
+
+#ifdef _MSC_VER
+#pragma once
+#endif
+
+#include <assert.h>
+
+#ifdef _WIN32
+
+#ifndef WINVER
+#define WINVER 0x0800
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0800
+#endif
+
+#ifndef _WIN32_DCOM
+#define _WIN32_DCOM
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0800
+#endif
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0700
+#endif
+
+#ifndef __MSVCRT_VERSION__
+#define __MSVCRT_VERSION__ 0x0A00
+#endif
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define ZuExport_API __declspec(dllexport)
+#define ZuExport_Explicit
+#define ZuImport_API __declspec(dllimport)
+#define ZuImport_Explicit extern
+
+#ifdef ZU_EXPORTS
+#define ZuAPI ZuExport_API
+#define ZuExplicit ZuExport_Explicit
+#else
+#define ZuAPI ZuImport_API
+#define ZuExplicit ZuImport_Explicit
+#endif
+#define ZuExtern extern ZuAPI
+
+#else /* _WIN32 */
+
+#define ZuAPI
+#define ZuExplicit
+#define ZuExtern extern
+
+#endif /* _WIN32 */
+
+// sanity check platform
+#include <limits.h>
+#if CHAR_BIT != 8
+#error "Broken platform - CHAR_BIT is not 8 - a byte is not 8 bits!"
+#endif
+#if UINT_MAX < 0xffffffff
+#error "Broken platform - UINT_MAX is < 0xffffffff - an int is < 32 bits!"
+#endif
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __GNUC__
+
+#define ZuLikely(x) __builtin_expect(!!(x), 1)
+#define ZuUnlikely(x) __builtin_expect(!!(x), 0)
+
+#ifdef ZDEBUG
+#define ZuInline inline
+#define ZuNoInline inline
+#else
+#define ZuInline inline __attribute__((always_inline))
+#define ZuNoInline inline __attribute__((noinline))
+#endif
+
+#else
+
+#define ZuLikely(x) (x)
+#define ZuUnlikely(x) (x)
+
+#ifdef _MSC_VER
+#define ZuInline __forceinline
+#else
+#define ZuInline inline
+#endif
+#define ZuNoInline
+
+#endif
+
+#ifdef __GNUC__
+#define ZuMayAlias(x) __attribute__((__may_alias__)) x
+#else
+#define ZuMayAlias(x) x
+#endif
+
+#if defined(linux) || defined(__mips64)
+#include <endian.h>
+#if __BYTE_ORDER == __BIG_ENDIAN
+#define Zu_BIGENDIAN 1
+#else
+#define Zu_BIGENDIAN 0
+#endif
+#else
+#ifdef _WIN32
+#define Zu_BIGENDIAN 0
+#endif
+#endif
+
+// alloca() alias
+
+#ifdef _MSC_VER
+#define ZuAlloca(n) _alloca(n)
+#else
+#ifndef _WIN32
+#include <alloca.h>
+#endif
+#define ZuAlloca(n) alloca(n)
+#endif
+
+#endif /* zu_lib_H */
